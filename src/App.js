c2f592d822ce795cbb24a14ecb2d0bd7a550742d
Abylin Johnson <jgabylin@karunya.edu.in>
2021-06-16 20:55:48 +0530

    Broken the website into smaller components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.components';
+import HomePage from './pages/homepage/homepage.component.jsx';
 
 function App() {
   return (
